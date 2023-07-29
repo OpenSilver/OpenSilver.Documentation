795f2705c11dc888d6ae448f1c85a4619b36603a
Fabio Silva <fabio.silva@userware.onmicrosoft.com>
2023-07-29 19:01:49 +1000

    Automated Tests on OpenSilver with Selenium

---
+++ examples/OpenSilverSeleniumDemo/OpenSilverSeleniumDemo.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverSeleniumDemo.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
