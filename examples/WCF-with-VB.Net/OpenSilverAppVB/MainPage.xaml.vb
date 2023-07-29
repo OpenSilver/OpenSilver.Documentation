2694f0009b83638bda36f264167da8a7c7ab05c8
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-07-29 17:46:51 +1000

    Use WCF service with Opensilver VB.Net Projects

---
+++ examples/WCF-with-VB.Net/OpenSilverAppVB/MainPage.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+Imports System.Windows
+Imports System.Windows.Controls
+
+Partial Public Class MainPage
+    Inherits Page
+    Public Sub New()
+        Me.InitializeComponent()
+        Main()
+    End Sub
+
+    Public Async Sub Main()
+        Await MyFunc()
+    End Sub
+
+    Public Async Function MyFunc() As Task(Of Integer)
+        Dim proxy = New OpenSilverServiceProxy.ServiceProxy()
+
+        Dim results = Await proxy.GetDataAsync(1)
+        Console.WriteLine(results)
+    End Function
+
+End Class
---
+++ examples/WCF-with-VB.Net/OpenSilverServiceProxy/ServiceProxy.cs
@@ -0,0 +1,20 @@
+﻿using ServiceReference1;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenSilverServiceProxy
+{
+    public class ServiceProxy
+    {
+        private readonly Service1Client _serviceClient;
+        public ServiceProxy()
+        {
+            _serviceClient = new Service1Client();
+        }
+
+        public async Task<string> GetDataAsync(Int32 id)
+        {
+            return await _serviceClient.GetDataAsync(id);
+        }
+    }
+}
